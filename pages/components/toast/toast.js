8ef9f63fe6644801976d5921061887fe701f344b
Manish Devrani <manishdevrani777@gmail.com>
2022-02-09 17:42:38 +0530

    Feat- Added toast fade animation

--- pages/components/toast/toast.js
+++ pages/components/toast/toast.js
@@ -1,10 +1,15 @@
 const showToastButton = document.querySelectorAll('.show-toast');
+const allToasts = document.querySelectorAll('.toast');
 
 const showToastInView = e => {
     let targetElement = e.target;
     let targetToast = targetElement.nextElementSibling;
+    allToasts.forEach(toast => {
+        toast.classList.add('toast--hide');
+    })
     targetToast.classList.remove('toast--hide');
     hideToastAfterThreeSec(targetToast);
+    targetToast.classList.add('fade-animation');
 }
 
 const hideToastAfterThreeSec = element => {

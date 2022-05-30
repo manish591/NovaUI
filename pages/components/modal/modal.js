cba7214155804311688e4bf6e7572c4472e5720c
Manish Devrani <manishdevrani777@gmail.com>
2022-05-30 23:23:16 +0530

    added overlay in modals

--- pages/components/modal/modal.js
+++ pages/components/modal/modal.js
@@ -2,24 +2,23 @@ const showModal = document.querySelectorAll('.show-modal');
 const clearModalBtn = document.querySelectorAll('.dialog__clear');
 const body = document.querySelector('body');
 
+const showModalInView = (e) => {
+  let target = e.target;
+  let targetModal = target.nextElementSibling;
+  targetModal.classList.remove('dialog--hide');
+  targetModal.classList.add('scale-dialog');
+};
 
-const showModalInView = e => {
-    let target = e.target;
-    let targetModal = target.nextElementSibling;
-    targetModal.classList.remove('dialog--hide');
-    targetModal.classList.add('scale-dialog');
-}
+const hideModalInView = (e) => {
+  let target = e.target;
+  let targetModal = target.parentElement.parentElement.parentElement;
+  targetModal.classList.add('dialog--hide');
+};
 
-const hideModalInView = e => {
-    let target = e.target;
-    let targetModal = target.parentElement.parentElement;
-    targetModal.classList.add('dialog--hide');
-}
+clearModalBtn.forEach((btn) => {
+  btn.addEventListener('click', hideModalInView);
+});
 
-clearModalBtn.forEach(btn => {
-    btn.addEventListener('click', hideModalInView);
-})
-
-showModal.forEach(btn => {
-    btn.addEventListener('click', showModalInView);
-})
+showModal.forEach((btn) => {
+  btn.addEventListener('click', showModalInView);
+});
